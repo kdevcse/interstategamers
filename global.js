472f7a240d2dd59a30cdd6f14e97b3ea8e12fc27
klewiscse <klewiscse@gmail.com>
2019-07-03 02:06:41 -0500

    Add Projects + Info pages/Fix bugs

---
+++ global.js
@@ -0,0 +1,8 @@
+function menuDropdown(){
+	var nav = document.getElementById("mobile-nav-list");
+	if (nav.style.display === "block") {
+	  nav.style.display = "none";
+	} else {
+	  nav.style.display = "block";
+	}
+}
--- home_episodes.js
+++ home_episodes.js
@@ -177,12 +177,3 @@ function transition(ele,val){
 		}
 	}
 }
-
-function menuDropdown(){
-	var nav = document.getElementById("mobile-nav-list");
-	if (nav.style.display === "block") {
-	  nav.style.display = "none";
-	} else {
-	  nav.style.display = "block";
-	}
-}
