5872296eb434b3e329be8f0e412032751e0224a0
klewiscse <klewiscse@gmail.com>
2019-07-10 02:17:28 -0500

    Add menu cancel button/fix episode hover

--- global.js
+++ global.js
@@ -1,8 +1,13 @@
 function menuDropdown(){
-	var nav = document.getElementById("mobile-nav-list");
+	let nav = document.getElementById("mobile-nav-list");
+	let icon = document.getElementById("mobile-nav-icon");
 	if (nav.style.display === "block") {
 	  nav.style.display = "none";
+	  icon.classList.remove("fa-times");
+	  icon.classList.add("fa-bars");
 	} else {
 	  nav.style.display = "block";
+	  icon.classList.remove("fa-bars");
+	  icon.classList.add("fa-times");
 	}
 }
