c1e6f1fd8af276b32e8c2e4002570576941a7326
klewiscse <klewiscse@gmail.com>
2019-08-04 17:44:07 -0500

    Add Twitch Live Functionality

--- Extras/extras.js
+++ Extras/extras.js
@@ -1,12 +1,18 @@
-function populateTwitchLinks(){
-    let twitchHovers = document.getElementsByClassName("twitch-link");
-    
-    for(let i = 0; i < twitchHovers.length; i++){
-        twitchHovers[i].addEventListener("mouseover", function(){
-            this.getElementsByClassName("fa-play")[0].classList.add("hovered");
-        });
-        twitchHovers[i].addEventListener("mouseout", function(){
-            this.getElementsByClassName("fa-play")[0].classList.remove("hovered");
-        });
+function getLiveStatus(){
+    let channels = document.getElementsByClassName("twitch-link");
+    for(let i = 0; i < channels.length; i++){
+        let xmlhttp = new XMLHttpRequest();
+        let channel = channels[i].getAttribute("channel");
+        xmlhttp.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var data = JSON.parse(this.responseText)["data"];
+                if(data.length > 0 && data[0]["type"] === "live"){
+                    channels[i].getElementsByClassName("live-text")[0].style.visibility = "visible";
+                }
+            }
+        };
+        xmlhttp.open("GET", `https://api.twitch.tv/helix/streams?user_login=${channel}`, true);
+        xmlhttp.setRequestHeader("Client-ID", "xjah8vp8zd8s7es9blv3jlxkst315r");
+        xmlhttp.send();
     }
 }
