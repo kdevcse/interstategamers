707a7f0f7aa71f6cfa5041df031e596c201654f3
Kevin <klewiscse@gmail.com>
2019-10-08 01:07:18 -0500

    Configure typescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,23 @@
-const { watch, series } = require('gulp');
+var gulp = require('gulp');
+var ts = require('gulp-typescript');
+var tsProject = ts.createProject('tsconfig.json');
 var exec = require('child_process').exec;
 
-function update(cb){
-    exec('python3 build/UpdateData.py ${PodId} ${PodKey} ${AirtableKey};', function (err, stdout, stderr) {
-        console.log(stdout);
-        console.log(stderr);
-        cb(err);
-      });
-    cb();
-
+function compileTs(cb){
+  tsProject.src()
+    .pipe(tsProject())
+    .js.pipe(gulp.dest('testDist'));
+  cb();
 }
 
-exports.update = series(update);
+function update(cb){
+  exec('python3 build/UpdateData.py ${PodId} ${PodKey} ${AirtableKey};', function (err, stdout, stderr) {
+      console.log(stdout);
+      console.log(stderr);
+      cb(err);
+    });
+  cb();
+}
+
+exports.update = gulp.series(update);
+exports.compile = gulp.series(compileTs);
---
+++ test/firstfolder/secondTest.ts
@@ -0,0 +1,4 @@
+function sup(compiler: string) {
+    console.log(`Sup from ${compiler}`);
+}
+sup('Compiler');
