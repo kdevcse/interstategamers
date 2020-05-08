e3c75060651758059a2bea90d481a2bc21926143
klewiscse <klewiscse@gmail.com>
2020-05-07 21:30:05 -0500

    Add styles back to home page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -16,15 +16,311 @@ export default {
 </script>
 
 <style>
-#nav {
-  padding: 30px;
+main {
+    display: grid;
+    grid-template-columns: 0.5fr 50%;
+    grid-template-areas: "left right";
+    align-items: center;
+    width: 100%;
+    padding: 20px 5%;
+    background-color: #2d32af;
 }
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
+#ig-main-left{
+    display: grid;
+    grid-area: left;
+    grid-gap: 20px;
+    align-items: center;
+}
+.ig-main-right-logo{
+    height: 375px;
+}
+.ig-main-left-text-container{
+    align-items: center;
+    color: white;
+}
+#ig-main-left-trailer{
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+#ig-main-left-trailer:hover{
+    cursor: pointer;
+    text-decoration: underline;
+}
+#ig-icons > a > svg:hover{
+    cursor: pointer;
+    opacity: 0.8;
+}
+#ig-icons {
+    margin-top: 10px;
+}
+#ig-icons > a{
+    text-decoration: none;
+}
+#ig-icons > a > svg{
+  color: white;
+  margin-right: 1.1875%;
+  margin-left: 1.1875%;
+  transition: opacity .2s ease-in;
+}
+.ig-content{
+  display: grid;
+}
+.episode, .episode-first {
+  display: grid;
+  align-items: center;
+  grid-template-columns: 50px auto;
+  width: 100%;
+  padding-top: 20px;
+  padding-bottom: 20px;
+  overflow: hidden;
+}
+.episode {
+    border-top: #2d32af solid 1px;
+}
+.player > svg {
+    font-size: 32px;
+}
+.player > svg:hover{
+    cursor: pointer;
+}
+.season-title{
+    color: red;
+    width: 100%;
+    border-bottom: solid red;
+    margin-bottom: 0;
+}
+.episode-title {
+    color: #2d32af;
+    font-size: 1.3rem;
+    margin-bottom: 0;
+    margin-top: 0;
+}
+.episode-title > svg {
+    color: red;
+}
+.ep-description {
+    color: #2d32af;
+    margin-top: 5px;
+    margin-bottom: 5px;
+}
+#ig-content-rank{
+    margin-left: 5%;
+    height: 100%;
+    color: #2d32af;
+    display: none;
+}
+#ig-content-rank-game-title{
+    margin-bottom: 0;
+}
+#ig-content-rank-game-rank{
+    font-size: 17px;
+    margin-top: 5px;
+}
+.rank-header{
+    margin-top: 10px;
+    margin-bottom: 10px;
+}
+#ig-content-rank-game-overall{
+    color: red;
+}
+.progress-background{
+    background-color: #f1f1f1;
+    width: 100%;
+    border-radius: 4px;
+}
+.progress-foreground{
+    height: 33px;
+    background-color: #2d32af;
+    border-radius: 4px;
+}
+#ig-content-rank-progress-overall{
+    background-color: red;
+}
+/* Extra small devices (phones, 600px and down) */
+@media only screen and (max-width: 768px) {
+  main{
+    display: block;
+    text-align: center;
+    padding: 20px 5% 30px 5%;
+  }
+  #episodes {
+    width: 100%;
+    margin-top: 20px;
+  }
+  #ig-content-rank {
+    display: none;
+  }
+  .ig-content{
+    padding-left: 5%;
+    padding-right: 5%;
+    grid-template-columns: 100%;
+  }
+  #ig-main-right{
+    display: block;
+  }
+  #ig-main-left{
+    width: 100%;
+    grid-gap: 25px;
+    grid-template-columns: 100%;
+    justify-content: center;
+  }
+  .ig-main-right-logo{
+    height: 250px;
+    border-radius: 6px;
+  }
+  #ig-icons{
+    display: none;
+  }
+  .ig-main-left-text-container > h1{
+    display: none;
+  }
+  .ig-main-left-text-container > p{
+    margin-bottom: 0;
+    margin-top: 25px;
+    padding-left: 3%;
+    padding-right: 3%;
+  }
+  #ig-main-left-trailer{
+    color: #2d32af;
+    padding: 12px;
+    border-radius: 6px;
+    background-color: white;
+    justify-self: center;
+  }
+  #ig-main-left-trailer > svg{
+    margin-right: 8px;
+    font-size: 25px;
+    color: red;
+  }
 }
 
-#nav a.router-link-exact-active {
-  color: #42b983;
+/* Small devices (portrait tablets and large phones, 600px and up) */
+@media only screen and (min-width: 768px) {
+    main{
+        grid-template-columns: minmax(min-content,45%) 55%;
+        height: 500px;
+    }
+    #ig-main-right{
+        text-align: center;
+        justify-self: end;
+    }
+    .ig-main-right-logo{
+        height: 340px;
+        border-radius: 6px;
+    }
+  #episodes {
+    width: 100%;
+  }
+  #ig-content-rank {
+    display: none;
+  }
+  .player > svg {
+    font-size: 32px;
+  }
+    .ig-content{
+        padding: 50px 5%;
+    }
+    #ig-icons > a > svg{
+        font-size: 27px;
+    }
+    #ig-main-left{
+        align-self: center;
+    }
+    .ig-main-left-text-container > h1{
+        font-size: 34px;
+        margin-bottom: 5px;
+        margin-top: 0;
+    }
+    .ig-main-left-text-container > p{
+        margin: 0;
+        margin-top: 10px;
+        font-size: 17px;
+    }
+    #ig-main-left-trailer{
+        width: 225px;
+        color: #2d32af;
+        font-size: 15px;
+        padding: 7px;
+        border-radius: 4px;
+        background-color: white;
+    }
+    #ig-main-left-trailer > svg{
+        margin-right: 10px;
+        font-size: 23px;
+        color: red;
+    }
+}
+
+/* Large devices (laptops/desktops, 992px and up) */
+@media only screen and (min-width: 992px) {
+    main{
+        grid-template-columns: minmax(min-content,50%) 50%;
+    }
+    .ig-main-left-text-container > h1{
+        font-size: 45px;
+    }
+    .ig-main-left-text-container > p{
+        font-size: 21px;
+    }
+    #ig-main-left-trailer{
+        width: 275px;
+        color: #2d32af;
+        font-size: 20px;
+        padding: 15px;
+        background-color: white;
+    }
+    #ig-main-left-trailer > svg{
+        margin-right: 11px;
+        font-size: 27px;
+        color: red;
+    }
+    .ig-main-right-logo{
+        height: 375px;
+    }
+    #ig-icons > a > svg{
+        font-size: 30px;
+    }
+    .ig-content{
+        grid-template-columns: 50% 50%;
+        grid-template-rows: 90vh;
+    }
+    #episodes {
+        overflow-y: scroll;
+        overflow-x: hidden;
+        padding-right: 2%;
+    }
+    /* width */
+    #episodes::-webkit-scrollbar {
+        width: 10px;
+    }
+    /* Track */
+    #episodes::-webkit-scrollbar-track {
+        background: #f1f1f1;
+        border-radius: 30px;
+    }
+    /* Handle */
+    #episodes::-webkit-scrollbar-thumb {
+        background: red;
+        border-radius: 30px;
+    }
+    .episode:hover,.episode-first:hover{
+        background: #f1f1f1;
+    }
+  #ig-content-rank {
+    display: none;
+  }
+}
+/* Extra large devices (large laptops and desktops, 1200px and up) */
+@media only screen and (min-width: 1200px) {
+    main{
+        grid-template-columns: minmax(min-content,50%) 50%;
+    }
+    #ig-main-right{
+        justify-self: normal;
+    }
+    .ig-main-left-text-container > p{
+        font-size: 25px;
+    }
 }
 </style>
