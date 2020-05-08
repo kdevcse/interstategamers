6f50588e35e40b75f2bf60e2593f47dab5229bbd
Kevin <klewiscse@gmail.com>
2020-05-08 16:25:11 -0500

    Begin Ranking Info merge

--- build/update-data.js
+++ build/update-data.js
@@ -8,9 +8,14 @@ let episodes, rankings;
 
 api.getAirtableData('keyiAfm7QZhgG2nkV','Ratings','IG Score').then((data) => {
     rankings = data;
-    console.log(data);
     api.getSimplecastData('ce93694b-1ad6-421b-af90-5a35ac2d1430', 'eyJhcGlfa2V5IjoiMzBjZDYyYzJiMWJhODMwNzlkZmZmMzQ2NTdjMjlkMTUifQ==')
     .then((data) => {
+        episodes = data;
+        for(let i = 0; i < episodes.length; i++){
+            if (episodes[i].type === 'full'){
+                episodes[i]['Ranking Info']
+            }
+        }
         fs.writeFileSync('./src/database/data2.json', JSON.stringify(data, null, '\t'));
     });
 });
