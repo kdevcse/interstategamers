9ad7771047c401d6a59999d5a5fa607c01ed539f
klewiscse <klewiscse@gmail.com>
2020-06-26 18:11:59 -0500

    Implement 'show more' scroll indicators

--- src/components/RankingsOptions.vue
+++ src/components/RankingsOptions.vue
@@ -1,10 +1,10 @@
 <template>
     <th class="options-header" colspan="10">
         <input @input="search" id="options-searchbox" type="text" placeholder="Search" value="" autocomplete="off">
-        <div id="scroll-indicator">
-            <font-awesome-icon id="scroll-indicator-left" :icon="['fas', 'caret-square-left']" title="Scroll left to see more content"></font-awesome-icon>
+        <div v-bind:class="{show: showBothIndicators}" id="scroll-indicator">
+            <font-awesome-icon id="scroll-indicator-left" v-bind:class="{show: showLeftIndicator}" :icon="['fas', 'caret-square-left']" title="Scroll left to see more content"></font-awesome-icon>
             <span id="scroll-indicator-txt">Scroll for more</span>
-            <font-awesome-icon id="scroll-indicator-right" :icon="['fas', 'caret-square-right']" title="Scroll right to see more content"></font-awesome-icon>
+            <font-awesome-icon id="scroll-indicator-right" v-bind:class="{show: showRightIndicator}" :icon="['fas', 'caret-square-right']" title="Scroll right to see more content"></font-awesome-icon>
         </div>
     </th>
 </template>
@@ -14,13 +14,47 @@ import { Component, Prop, Vue } from 'vue-property-decorator'
 
 @Component
 export default class RankingsOptions extends Vue {
-    showIndicator() {
+    showLeftIndicator = false;
+    showRightIndicator = false;
+    showBothIndicators = false;
 
+    created() {
+        window.addEventListener('resize', this.showIndicator);
+        window.addEventListener('scroll', this.showIndicator);
+    };
+
+    destroyed() {
+        window.removeEventListener('resize', this.showIndicator);
+        window.removeEventListener('scroll', this.showIndicator);
+    };
+
+    showIndicator() {
+        const pos = window.scrollX;
+	    const maxWidth = document.documentElement.scrollWidth - document.documentElement.clientWidth;
+
+        if (pos > 0) {
+            this.showLeftIndicator = true;
+        } else {
+            this.showLeftIndicator = false;
+        }
+
+        if (pos !== maxWidth) {
+            this.showRightIndicator = true;
+        } else {
+            this.showRightIndicator = false;
+        }
+
+        if (this.showLeftIndicator || this.showRightIndicator) {
+            this.showBothIndicators = true;
+        }
+        else {
+            this.showBothIndicators = false;
+        }
     };
 
     search(e: any) {
         this.$emit('search-table', e.target.value);
-    }
+    };
 }
 </script>
 
@@ -48,14 +82,23 @@ export default class RankingsOptions extends Vue {
 	display: none;
 }
 #scroll-indicator{
+    visibility: hidden;
 	float: right;
 }
 #scroll-indicator-txt {
-    padding: 0px 5px;
+    margin: 0px 5px;
 }
-/*#scroll-indicator-left, #scroll-indicator-right{
+#scroll-indicator-left,
+#scroll-indicator-right {
 	visibility: hidden;
-}*/
+    height: 15px;
+    width: 15px;
+}
+#scroll-indicator-left.show,
+#scroll-indicator-right.show,
+#scroll-indicator.show{
+	visibility: visible;
+}
 /* Extra small devices (phones, 600px and down) */
 @media only screen and (max-width: 770px) {
 	.options-header{
