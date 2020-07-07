c0fb9ab62eb64a40d835682589b3f1084ffa5d74
klewiscse <klewiscse@gmail.com>
2020-07-06 23:57:34 -0500

    Remove Service Worker

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import App from './App.vue';
-import './registerServiceWorker';
 import router from './router';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faPlayCircle, faPauseCircle, faGamepad, faSortUp, faSortDown, 
