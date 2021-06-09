e3d778de31f2ed0e736017e8ec2b0afc49138f52
klewiscse <klewiscse@gmail.com>
2021-06-09 00:35:00 -0500

    Add firebase package

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
+import firebase from 'firebase/app';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faPlayCircle, faPauseCircle, faGamepad, faSortUp, faSortDown, 
         faUserPlus, faCaretSquareLeft, faCaretSquareRight, faBars, faTimes} from '@fortawesome/free-solid-svg-icons';
@@ -13,6 +14,20 @@ library.add(faPlayCircle, faPauseCircle, faGamepad, faSortUp,
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
 Vue.config.productionTip = true;
+
+/*Firebase Setup*/
+const firebaseConfig = {
+  apiKey: "AIzaSyBtt-MwRoY5x0ValrczH7bomtx5cyMvopc",
+  authDomain: "interstategamers-7122d.firebaseapp.com",
+  projectId: "interstategamers-7122d",
+  storageBucket: "interstategamers-7122d.appspot.com",
+  messagingSenderId: "960788987586",
+  appId: "1:960788987586:web:d1fe11bb12f8c6512d455b",
+  measurementId: "G-2342HJ4PT9"
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
 
 new Vue({
   router,
