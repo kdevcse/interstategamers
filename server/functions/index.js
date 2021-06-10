f856d6bd124852c6be34dc90724ea737aa161f6d
Kevin <klewiscse@gmail.com>
2021-06-10 03:08:36 -0500

    Write firebase function to update podcast data

--- server/functions/index.js
+++ server/functions/index.js
@@ -1,9 +1,77 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const fetch = require('node-fetch');
+//import admin from 'firebase-admin';
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+function getSimplecastData (url, podKey) {
+  const header = { 'authorization' : `Bearer ${podKey}` };
+
+	return fetch(url, {
+		method: 'get',
+		headers: header
+	})
+  .then(res => res.json())
+	.then(data => {
+    return data;
+    /*return { 
+      episodes: data.collection,
+      next: data.pages.next ? data.pages.next.href : null
+    };*/
+  }).catch((error) => {
+    functions.logger.info(`Error: ${error}`, { structuredData: true });
+  });
+}
+
+async function updatePodcastData(data) {
+  if (!data)
+    return;
+
+  await admin.firestore().collection('podcast-data').get().then((c) => {
+    for (let i = 0; i < data.length; i++) {
+      functions.logger.info(data[i].id);
+      if (!c.docs.some(doc => doc.id === data[i].id)) {
+        admin.firestore().collection('podcast-data').doc(data[i].id).set(data[i]);
+        functions.logger.info(`Added episode Id: ${data[i].id}`, { structuredData: true });
+      } else {
+        admin.firestore().collection('podcast-data').doc(data[i].id).update(data[i]);
+        functions.logger.info(`Updated episode Id: ${data[i].id}`, { structuredData: true });
+      }
+    }
+    return;
+  }).catch((error) => {
+    functions.logger.info(`Error: ${error}`, { structuredData: true });
+  });
+  
+}
+
+exports.updatePodcastData = functions.https.onRequest(async (request, response) => {
+  try { 
+    var apiKeysColl = await admin.firestore().collection('api-keys').get();
+
+    if (!apiKeysColl.docs[0])
+      return;
+
+    var apiKeys = apiKeysColl.docs[0].data();
+    var data = [];
+    var url = `https://api.simplecast.com/podcasts/${apiKeys.simplecast_id}/episodes`;
+    var responseData = await getSimplecastData(url, apiKeys.simplecast_key);
+    Array.prototype.push.apply(data, responseData.collection);
+
+    const total = responseData.pages.total;
+    var count = 0;
+    while (responseData && responseData.pages.next && count < total) {
+      // eslint-disable-next-line no-await-in-loop
+      responseData = await getSimplecastData(responseData.pages.next.href, apiKeys.simplecast_key);
+      Array.prototype.push.apply(data, responseData.collection);
+      count++;
+    }
+
+    updatePodcastData(data);
+
+    response.send('Podcast data updated');
+  } catch(e) {
+    response.send(`Error occurred while trying to update podcast data: ${e}`);
+  }
+});
