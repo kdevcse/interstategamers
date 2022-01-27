9060a037e2376e94b63d99b04742911a1f8639d7
Kev <klewiscse@gmail.com>
2022-01-27 04:30:18 -0600

    Fix spacing issues for code review

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,15 @@ import App from './App.vue';
 import router from './router';
 import firebase from 'firebase/app';
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faPlayCircle, faPauseCircle, faGamepad, faSortUp, faSortDown, 
-        faUserPlus, faCaretSquareLeft, faCaretSquareRight, faBars, faTimes} from '@fortawesome/free-solid-svg-icons';
+import {
+  faPlayCircle, faPauseCircle, faGamepad, faSortUp, faSortDown,
+  faUserPlus, faCaretSquareLeft, faCaretSquareRight, faBars, faTimes
+} from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 library.add(faPlayCircle, faPauseCircle, faGamepad, faSortUp,
-             faSortDown, faUserPlus, faCaretSquareLeft, faCaretSquareRight,
-             faBars, faTimes);
+  faSortDown, faUserPlus, faCaretSquareLeft, faCaretSquareRight,
+  faBars, faTimes);
 
 Vue.component('font-awesome-icon', FontAwesomeIcon);
 
