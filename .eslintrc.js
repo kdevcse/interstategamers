c2a726c9f9cc35d36f78e95dd91c268e041537e5
Kev <klewiscse@gmail.com>
2022-03-03 05:49:51 -0600

    Include naming convention linting options

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,7 +19,47 @@ module.exports = {
     "@typescript-eslint/space-before-blocks": ["error", "always"],
     "@typescript-eslint/type-annotation-spacing": ["error", { "after": true }],
     "space-before-function-paren": "off",
-    "@typescript-eslint/space-before-function-paren": ["error", "never"]
+    "@typescript-eslint/space-before-function-paren": ["error", "never"],
+    "default-param-last": "off",
+    "@typescript-eslint/default-param-last": ["error"],
+    "camelcase": "off",
+    "@typescript-eslint/naming-convention": [
+      "warn",
+      {
+        "selector": "default",
+        "format": ["camelCase"]
+      },
+      {
+        "selector": "variable",
+        "modifiers": ["global", "const", "exported"],
+        "format": ["UPPER_CASE"]
+      },
+      {
+        "selector": "variableLike",
+        "format": ["camelCase", "PascalCase"]
+      },
+      {
+        "selector": "interface",
+        "format": ["PascalCase"],
+        "prefix": ["I"]
+      },
+      {
+        "selector": "typeLike",
+        "format": ["PascalCase"]
+      },
+      {
+        "selector": "enum",
+        "format": ["PascalCase"]
+      },
+      {
+        "selector": "enumMember",
+        "format": ["UPPER_CASE"]
+      },
+      {
+        "selector": "objectLiteralProperty",
+        "format": ["PascalCase", "camelCase"]
+      }
+    ]
   },
   env: {
     "vue/setup-compiler-macros": true
