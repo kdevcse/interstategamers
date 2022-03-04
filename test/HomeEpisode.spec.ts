717f7d795d04fe68bab16fd8c51dd8a4747c77c9
Kevin <klewiscse@gmail.com>
2022-03-04 16:36:06 -0600

    Finsih HomeEpisode unit tests

--- test/HomeEpisode.spec.ts
+++ test/HomeEpisode.spec.ts
@@ -1,7 +1,7 @@
-import { useAudio } from "@/components/HomeEpisode";
+import { useAudio, useSendScore } from "@/components/HomeEpisode";
 
 describe("HomeEpisode unit tests", () => {
-  test("useAudio composable", () => {
+  test("useAudio composable with audio element", () => {
     let playCount = 0;
     let pauseCount = 0;
 
@@ -25,5 +25,48 @@ describe("HomeEpisode unit tests", () => {
     expect(playing.value).toBe(false);
     expect(playCount).toBe(1);
     expect(pauseCount).toBe(1);
+  });
+
+  test("useAudio composable with audio element", () => {
+    const { playing, play } = useAudio(null as any);
+
+    expect(playing.value).toBe(false);
+    play();
+    expect(playing.value).toBe(false);
+  });
+
+  test("useSendScore composable with ranking id", () => {
+    const rankingId = "123sdsf";
+    let eventName = "";
+    let eventValues: Array<string> = [];
+
+    const emit = (name: string, vals: Array<string>) => {
+      eventName = name;
+      eventValues = vals;
+    };
+
+    const { sendScore } = useSendScore(emit);
+
+    sendScore(rankingId);
+
+    expect(eventName).toBe("show-score");
+    expect(eventValues[0]).toBe(rankingId);
+  });
+
+  test("useSendScore composable without ranking id", () => {
+    let eventName = "";
+    let eventValues: Array<string> = [];
+
+    const emit = (name: string, vals: Array<string>) => {
+      eventName = name;
+      eventValues = vals;
+    };
+
+    const { sendScore } = useSendScore(emit);
+
+    sendScore(undefined);
+
+    expect(eventName).toBe("");
+    expect(eventValues[0]).toBe(undefined);
   });
 });
