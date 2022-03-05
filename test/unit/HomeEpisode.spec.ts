7301fe1092fdba2367235621f1d15c9a6000149e
Kev <klewiscse@gmail.com>
2022-03-04 22:28:37 -0600

    update HomeEpisode test quite structure

--- test/unit/HomeEpisode.spec.ts
+++ test/unit/HomeEpisode.spec.ts
@@ -1,72 +1,76 @@
 import { useAudio, useSendScore } from "@/components/HomeEpisode";
 
 describe("HomeEpisode unit tests", () => {
-  test("useAudio composable with audio element", () => {
-    let playCount = 0;
-    let pauseCount = 0;
+  describe("useAudio composable", () => {
+    test("With audio element", () => {
+      let playCount = 0;
+      let pauseCount = 0;
 
-    const { playing, play } = useAudio({
-      play: () => {
-        playCount++;
-      },
-      pause: () => {
-        pauseCount++;
-      }
-    } as any);
+      const { playing, play } = useAudio({
+        play: () => {
+          playCount++;
+        },
+        pause: () => {
+          pauseCount++;
+        },
+      } as any);
 
-    expect(playing.value).toBe(false);
-    expect(playCount).toBe(0);
-    expect(pauseCount).toBe(0);
-    play();
-    expect(playing.value).toBe(true);
-    expect(playCount).toBe(1);
-    expect(pauseCount).toBe(0);
-    play();
-    expect(playing.value).toBe(false);
-    expect(playCount).toBe(1);
-    expect(pauseCount).toBe(1);
-  });
+      expect(playing.value).toBe(false);
+      expect(playCount).toBe(0);
+      expect(pauseCount).toBe(0);
+      play();
+      expect(playing.value).toBe(true);
+      expect(playCount).toBe(1);
+      expect(pauseCount).toBe(0);
+      play();
+      expect(playing.value).toBe(false);
+      expect(playCount).toBe(1);
+      expect(pauseCount).toBe(1);
+    });
 
-  test("useAudio composable with audio element", () => {
-    const { playing, play } = useAudio(null as any);
+    test("Without audio element", () => {
+      const { playing, play } = useAudio(null as any);
 
-    expect(playing.value).toBe(false);
-    play();
-    expect(playing.value).toBe(false);
-  });
+      expect(playing.value).toBe(false);
+      play();
+      expect(playing.value).toBe(false);
+    });
 
-  test("useSendScore composable with ranking id", () => {
-    const rankingId = "123sdsf";
-    let eventName = "";
-    let eventValues: Array<string> = [];
+    describe("useSendScore composable", () => {
+      test("With ranking id", () => {
+        const rankingId = "123sdsf";
+        let eventName = "";
+        let eventValues: Array<string> = [];
 
-    const emit = (name: string, vals: Array<string>) => {
-      eventName = name;
-      eventValues = vals;
-    };
+        const emit = (name: string, vals: Array<string>) => {
+          eventName = name;
+          eventValues = vals;
+        };
 
-    const { sendScore } = useSendScore(emit);
+        const { sendScore } = useSendScore(emit);
 
-    sendScore(rankingId);
+        sendScore(rankingId);
 
-    expect(eventName).toBe("show-score");
-    expect(eventValues[0]).toBe(rankingId);
-  });
+        expect(eventName).toBe("show-score");
+        expect(eventValues[0]).toBe(rankingId);
+      });
 
-  test("useSendScore composable without ranking id", () => {
-    let eventName = "";
-    let eventValues: Array<string> = [];
+      test("Without ranking id", () => {
+        let eventName = "";
+        let eventValues: Array<string> = [];
 
-    const emit = (name: string, vals: Array<string>) => {
-      eventName = name;
-      eventValues = vals;
-    };
+        const emit = (name: string, vals: Array<string>) => {
+          eventName = name;
+          eventValues = vals;
+        };
 
-    const { sendScore } = useSendScore(emit);
+        const { sendScore } = useSendScore(emit);
 
-    sendScore(undefined);
+        sendScore(undefined);
 
-    expect(eventName).toBe("");
-    expect(eventValues[0]).toBe(undefined);
+        expect(eventName).toBe("");
+        expect(eventValues[0]).toBe(undefined);
+      });
+    });
   });
 });
