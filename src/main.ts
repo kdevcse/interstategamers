106cb8d87e122f270473cb288b05edd52f69728f
Kev <kdevcse@protonmail.com>
2024-02-07 14:28:46 -0600

    Remove Patreon reference in main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import ROUTER from "./router";
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faFacebookSquare, faTwitter, faPatreon, faInstagram} from "@fortawesome/free-brands-svg-icons";
+import { faFacebookSquare, faTwitter, faInstagram} from "@fortawesome/free-brands-svg-icons";
 import {
   faPlayCircle, faPauseCircle, faGamepad, faSortUp, faSortDown,
   faUserPlus, faCaretSquareLeft, faCaretSquareRight, faBars, faTimes, faRss
@@ -13,7 +13,7 @@ const app = createApp(App as any);
 
 library.add(faPlayCircle, faPauseCircle, faGamepad, faSortUp,
   faSortDown, faUserPlus, faCaretSquareLeft, faCaretSquareRight,
-  faBars, faTimes, faFacebookSquare, faTwitter, faPatreon, faInstagram,
+  faBars, faTimes, faFacebookSquare, faTwitter, faInstagram,
   faRss);
 
 app.component("FontAwesomeIcon", FontAwesomeIcon);
